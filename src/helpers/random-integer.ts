8d37aa055b1e6a183f3a249cdff2262e5f9f075d
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-07-29 22:13:04 +0530

    helper: random-integer

---
+++ src/helpers/random-integer.ts
@@ -0,0 +1,9 @@
+/**
+ * This will return an integer value between min and max both inclusive.
+ * @param {number} min The starting integer value.
+ * @param {number} max The ending integer value.
+ * @returns {number} Random integer value between min and max both inclusive.
+ */
+export default function randomInteger(min: number, max: number) {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+}
