ecb75270ae5119bd99c2df4f7cedae7f33079d3c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-07-29 22:01:33 +0530

    helper - random-float

---
+++ src/helpers/random-float.ts
@@ -0,0 +1,18 @@
+import { DEFAULT_NUMBER_OF_DECIMAL_PLACE } from '../constants';
+
+/**
+ * This will return a float value between min and max both inclusive.
+ * @param {number} min The starting float value.
+ * @param {number} max The ending float value.
+ * @param {number} decimals The decimal place.
+ * @returns {number} Random float value between min and max both inclusive.
+ */
+export default function randomFloat(
+  min: number,
+  max: number,
+  decimals: number = DEFAULT_NUMBER_OF_DECIMAL_PLACE
+) {
+  return parseFloat(
+    (Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min)).toFixed(decimals)
+  );
+}
